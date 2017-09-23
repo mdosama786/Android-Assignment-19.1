3061952b0d0bfb1c158d48f1f8755992879eb2c6
Mohammad Osama <mdosama.ansar@gmail.com>
2017-09-23 12:52:16 +0530

    Android-Assignment-19.1
    included screenshot and apk file

---
+++ app/src/main/java/com/example/osama/assignment191/network/OnWebServiceResult.java
@@ -0,0 +1,9 @@
+package com.example.osama.assignment191.network;
+
+
+import com.example.osama.assignment191.utils.CommonUtilities;
+
+
+public interface OnWebServiceResult {
+	public void getWebResponse(String result, CommonUtilities.SERVICE_TYPE type);
+}
---
+++ app/src/main/java/com/example/osama/assignment191/utils/CommonUtilities.java
@@ -0,0 +1,10 @@
+package com.example.osama.assignment191.utils;
+
+
+public class CommonUtilities {
+
+    public enum SERVICE_TYPE {
+        GET_DATA, ERROR_REPORT
+
+    }
+}
